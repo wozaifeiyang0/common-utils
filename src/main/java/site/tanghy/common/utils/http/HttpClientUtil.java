19b97c5e0635a26b93b78d2f67f183e0518952b4
434532756 <434532756@qq.com>
2020-03-08 14:38:07 +0800

    添加工具注释

--- src/main/java/site/tanghy/common/utils/http/HttpClientUtil.java
+++ src/main/java/site/tanghy/common/utils/http/HttpClientUtil.java
@@ -1,20 +1,24 @@
 package site.tanghy.common.utils.http;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.ServiceUnavailableRetryStrategy;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.config.ConnectionConfig;
-import org.apache.http.impl.client.DefaultServiceUnavailableRetryStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.protocol.HttpContext;
 import site.tanghy.common.utils.ObjectUtil;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
- *
  * 创建可重试请求对象
+ *
  * @author tanghy
  */
 public class HttpClientUtil {
@@ -23,51 +27,142 @@ public class HttpClientUtil {
 
     }
 
-    public HttpClientBuilder builder(int retryNum, int interval) {
-        HttpClientBuilder builder = builder(retryNum, interval, true);
+    /**
+     * @param retryNum
+     * @param interval
+     * @return
+     */
+    public static HttpClientBuilder builder(int retryNum, int interval) {
+        HttpClientBuilder builder = builder(retryNum, interval, true, null);
         return builder;
     }
 
-    public HttpClientBuilder builder(int retryNum, int interval, boolean isPool) {
+    /**
+     * @param retryNum
+     * @param interval
+     * @param isPool
+     * @param requestConfig
+     * @return
+     */
+    public static HttpClientBuilder builder(int retryNum, int interval, boolean isPool, RequestConfig requestConfig) {
+
+        RequestConfig.Builder reqBuilder = RequestConfig.custom();
+        reqBuilder.setExpectContinueEnabled(true);
+        HttpClientBuilder builder = getBuilder(retryNum, interval, isPool);
+        builder.setDefaultRequestConfig(reqBuilder.build());
+        if (ObjectUtil.isNotEmpty(requestConfig))
+            builder.setDefaultRequestConfig(requestConfig);
+
+        return builder;
+    }
+
+
+    public static HttpClientBuilder builder(int retryNum, int interval, boolean isPool, int socketTimeout, int connectTimeout, int requestTimeout) {
+
+        RequestConfig.Builder reqBuilder = RequestConfig.custom();
+        reqBuilder.setSocketTimeout(socketTimeout);
+        reqBuilder.setConnectTimeout(connectTimeout);
+        reqBuilder.setConnectionRequestTimeout(requestTimeout);
+        reqBuilder.setExpectContinueEnabled(true);
+
+        HttpClientBuilder builder = getBuilder(retryNum, interval, isPool);
+        builder.setDefaultRequestConfig(reqBuilder.build());
+
+        return builder;
+    }
+
+    /**
+     * @param retryNum
+     * @param interval
+     * @param cm
+     * @param requestConfig
+     * @param connectionConfig
+     * @return
+     */
+    public static HttpClientBuilder builder(int retryNum, int interval, PoolingHttpClientConnectionManager cm, RequestConfig requestConfig, ConnectionConfig connectionConfig) {
+
+        HttpClientBuilder builder = setRetryHandler(retryNum, interval);
+
+        if (ObjectUtil.isNotEmpty(connectionConfig)) {
+            builder.setDefaultConnectionConfig(connectionConfig);
+        }
+        if (ObjectUtil.isNotEmpty(requestConfig)) {
+            builder.setDefaultRequestConfig(requestConfig);
+        }
+        if (ObjectUtil.isNotEmpty(cm)) {
+            builder.setConnectionManager(cm);
+        }
+
+        return builder;
+    }
+
+    /**
+     * @param retryNum
+     * @param interval
+     * @param isPool
+     * @return
+     */
+    private static HttpClientBuilder getBuilder(int retryNum, int interval, boolean isPool) {
 
         Charset charset = StandardCharsets.UTF_8;
         ConnectionConfig.Builder connBuilder = ConnectionConfig.custom().setCharset(charset);
 
-        RequestConfig.Builder reqBuilder = RequestConfig.custom();
-//        reqBuilder.setExpectContinueEnabled(false);
-        reqBuilder.setSocketTimeout(10 * 1000);
-        reqBuilder.setConnectTimeout(10 * 1000);
-        reqBuilder.setConnectionRequestTimeout(10 * 1000);
-//        reqBuilder.setMaxRedirects(10);
-
-        ServiceUnavailableRetryStrategy retryStrategy = new DefaultServiceUnavailableRetryStrategy(retryNum, interval);
+        HttpClientBuilder builder = setRetryHandler(retryNum, interval);
 
         PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
         cm.setMaxTotal(1000);
         cm.setDefaultMaxPerRoute(1000);
 
-        HttpClientBuilder builder = HttpClients.custom();
-        builder.setServiceUnavailableRetryStrategy(retryStrategy);
         builder.setDefaultConnectionConfig(connBuilder.build());
-        builder.setDefaultRequestConfig(reqBuilder.build());
         //是否使用连接池
-        if (isPool)
+        if (isPool) {
             builder.setConnectionManager(cm);
+        }
 
         return builder;
     }
 
-    public HttpClientBuilder builder(int retryNum, int interval, PoolingHttpClientConnectionManager cm, RequestConfig requestConfig, ConnectionConfig connectionConfig) {
+    private static HttpClientBuilder setRetryHandler(int retryNum, int interval) {
 
-        ServiceUnavailableRetryStrategy retryStrategy = new DefaultServiceUnavailableRetryStrategy(retryNum, interval);
+        HttpRequestRetryHandler httpRequestRetryHandler = (e, executeNum, httpContext) -> {
+
+            if (ObjectUtil.isNotEmpty(e)) {
+                if (executeNum > retryNum) {
+                    return false;
+                } else {
+                    System.out.println("重试第：" + executeNum + " 次。");
+                    return true;
+                }
+            } else {
+                return false;
+            }
+
+        };
+
+        ServiceUnavailableRetryStrategy retryStrategy = new ServiceUnavailableRetryStrategy() {
+            @Override
+            public boolean retryRequest(HttpResponse httpResponse, int executeNum, HttpContext httpContext) {
+
+                StatusLine statusLine = httpResponse.getStatusLine();
+                if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                    return false;
+                } else if (executeNum > retryNum) {
+                    return false;
+                } else {
+                    System.out.println("超时重试第：" + executeNum + " 次。");
+                    return true;
+                }
+            }
+
+            @Override
+            public long getRetryInterval() {
+                return interval;
+            }
+        };
 
         HttpClientBuilder builder = HttpClients.custom();
+        builder.setRetryHandler(httpRequestRetryHandler);
         builder.setServiceUnavailableRetryStrategy(retryStrategy);
-        builder.setDefaultConnectionConfig(connectionConfig);
-        builder.setDefaultRequestConfig(requestConfig);
-        //是否使用连接池
-        if (ObjectUtil.isNotEmpty(cm))
-            builder.setConnectionManager(cm);
 
         return builder;
     }
---
+++ src/test/java/site/tanghy/common/utils/http/HttpClientUtilTest.java
@@ -0,0 +1,37 @@
+package site.tanghy.common.utils.http;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author tanghy
+ * @description
+ * @date 2020-03-08 10:43 上午
+ */
+class HttpClientUtilTest {
+
+    @Test
+    void builder() throws IOException {
+        HttpClientBuilder builder = HttpClientUtil.builder(5, 3000);
+        CloseableHttpClient build = builder.build();
+        HttpGet httpGet = new HttpGet("http://localhost:8080/test/long?name=5");
+        HttpResponse response = build.execute(httpGet);
+        String res = EntityUtils.toString(response.getEntity());
+        System.out.println(res);
+
+
+    }
+
+    @Test
+    void testBuilder() {
+    }
+}
